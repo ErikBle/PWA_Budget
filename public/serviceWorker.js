d1b9c2883d9fc971a5e17aa5fdb9215303fcc29f
Yfopla <ekblecker@gmail.com>
2020-06-20 09:56:27 -0400

    Fixed: console logs in app

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -11,7 +11,6 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);
       })
     );
